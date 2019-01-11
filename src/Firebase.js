4aea969fb3ea8a09a1b24f6335175437be552d21
fcsjogreen <fiocsj@gmail.com>
2019-01-11 11:15:36 -0500

    seguridad

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,14 +3,7 @@ import firestore from 'firebase/firestore';
 
 const settings = {timestampsInSnapshots: true};
 
-const config = {
-    apiKey: "AIzaSyCZspbXsxdvAZEjYnUxqLoWves0IeuPeFU",
-    authDomain: "bodega-e4925.firebaseapp.com",
-    databaseURL: "https://bodega-e4925.firebaseio.com",
-    projectId: "bodega-e4925",
-    storageBucket: "bodega-e4925.appspot.com",
-    messagingSenderId: "197587334934"
-};
+
 
 firebase.initializeApp(config);
 
